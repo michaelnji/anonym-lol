e5c6b6a783335a9ee10e983a926143e8e2d47004
COD3VIL <codebase65@outlook.com>
2024-07-12 20:33:21 +0100

    feat: 💄 Added bottom Navbar and minor improvement to ui

--- src/lib/ui/components/navbar.svelte
+++ src/lib/ui/components/navbar.svelte
@@ -1,26 +1,34 @@
 <script lang="ts">
+    import { page } from "$app/stores";
+
+    // biome-ignore lint/suspicious/noConfusingLabels: <explanation>
+    $: currentPath = $page.url.pathname
 </script>
 
 <div class="navbar-container">
     <nav class="navbar">
         <a href="/">
-            <div class="navbar-item">
-                <i class="bx  bx-home-alt"></i>
+            <div class="navbar-item" class:active={currentPath === '/'}>
+                <i class=" bx bx-home-alt"></i>
+                <div class="indicator" class:visually-hidden={currentPath !== '/'}></div>
             </div>
         </a>
         <a href="/messages">
-            <div class="navbar-item">
-                <i class="bx  bx-message-rounded"></i>
+            <div class="navbar-item" class:active={currentPath === '/messages'}>
+                <i class="bx bx-message-rounded"></i>
+                <div class="indicator"  class:visually-hidden={currentPath !== '/messages'}></div>
             </div>
         </a>
-        <a href="/messages">
-            <div class="navbar-item">
-                <i class="bx  bx-bar-chart-alt-2"></i>
+        <a href="/stats">
+            <div class="navbar-item" class:active={currentPath === '/stats'}>
+                <i class="bx bx-bar-chart-alt-2"></i>
+                <div class="indicator"  class:visually-hidden={currentPath !== '/stats'}></div>
             </div>
         </a>
-        <a href="/messages">
-            <div class="navbar-item">
-                <i class="bx  bx-user"></i>
+        <a href="/profile">
+            <div class="navbar-item" class:active={currentPath === '/profile'}>
+                <i class="bx bx-user"></i>
+                <div class="indicator"  class:visually-hidden={currentPath !== '/profile'}></div>
             </div>
         </a>
     </nav>
@@ -38,7 +46,7 @@
         background: var(--base-50);
         border-top-left-radius: var(--border-radius);
         border-top-right-radius: var(--border-radius);
-      
+
         margin: 0 auto;
         @media (min-width: 819px) {
             display: none;
@@ -48,11 +56,39 @@
             justify-content: space-between;
             align-items: center;
             gap: 0 5px;
-            padding:  20px;
-            padding-bottom: 10px;
-            a{
-                  color: var(--base-content);
-                  font-size: var(--fs-md);
+            padding: 15px 15px 0 15px;
+
+            a {
+                color: var(--base-content);
+                font-size: var(--fs-lg);
+                width: 100%;
+                .navbar-item {
+                    width: 100%;
+                    display: grid;
+                    place-items: center;
+                    gap: 6px 0;
+                    border-radius: var(--border-radius);
+                    transition:
+                        background var(--duration) var(--easing),
+                        color var(--duration) var(--easing);
+                    .indicator {
+                         transition:
+                        all var(--duration) var(--easing);
+                       
+                        width: 90%;
+                        padding: 3px;
+                        background-color: var(--primary);
+                        border-top-left-radius: var(--border-radius);
+                        border-top-right-radius: var(--border-radius);
+                    }
+                    /* &:hover {
+                        background: var(--base-200);
+                    } */
+                }
+                .active {
+                    color: var(--primary);
+                    
+                }
             }
         }
     }
